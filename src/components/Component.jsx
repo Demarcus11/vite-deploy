9828bc1c0664ed7ef521c30b861911dec59c79fc
Demarcus Hollman <demarcushollman1@gmail.com>
2023-05-18 11:43:42 -0400

    hello

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -2,7 +2,8 @@ export function Component() {
   return (
     <>
       <div>
-        <img src="../assets/bg-card-front.png" alt="" />
+        {/* <img src="../assets/bg-card-front.png" alt="" /> */}
+        <img src="../assets/react.svg" alt="" />
       </div>
     </>
   );
