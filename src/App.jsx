76cf7a482ce260c7bd7fe9407bf16c1d53cefb4b
Demarcus Hollman <demarcushollman1@gmail.com>
2023-05-18 11:21:42 -0400

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import cardFront from "./assets/bg-card-front.png";
-import { Component } from "./components/Component";
+import { Component } from "./Component";
 import "./App.css";
 
 export default function App() {
