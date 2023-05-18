ee4cf311b286103f177b68c1e0d5e709ef6b1365
Demarcus Hollman <demarcushollman1@gmail.com>
2023-05-18 10:41:32 -0400

    add: Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import cardFront from "./assets/bg-card-front.png";
+import Component from "./components/Component";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <div>
+        <Component />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
           <img src={cardFront} alt="" />
---
+++ src/components/Component.jsx
@@ -0,0 +1,11 @@
+import cardFront from "./assets/bg-card-front.png";
+
+export function Component() {
+  return (
+    <>
+      <div>
+        <img src={cardFront} alt="" />
+      </div>
+    </>
+  );
+}
